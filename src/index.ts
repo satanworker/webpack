52b8fafcbd20fb248d2755d6ba78e34a9d354040
satanworker <satanworker@gmail.com>
2016-04-13 01:15:03 +0300

    webpack

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { bootstrap } from 'angular2/platform/browser';
+import { provide } from 'angular2/core';
+import {WelcomeComponent} from './welcome/welcome.component';
+
+import './styl/test.styl';
+
+bootstrap(WelcomeComponent);
---
+++ src/welcome/welcome.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+@Component({
+    selector: 'welcome',
+    template: '<p>hello from webpack with stylus</p>'
+})
+export class WelcomeComponent {
+}
